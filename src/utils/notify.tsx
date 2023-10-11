c078c5d0837a03cd698b83ffac787fc6d70408e4
CelesteBloodreign <celeste.bloodreign@dddice.com>
2023-10-11 12:17:44 -0400

    fix(d&db): conflicts with homebrew creation menu

--- src/utils/notify.tsx
+++ src/utils/notify.tsx
@@ -15,21 +15,22 @@ const notify = message => {
   messages = [...messages, message];
   root.render(<Notification messages={messages} onRemoveMessage={onRemoveMessage} />);
 };
+let root;
+if (window == window.top) {
+  // insert notification panel
+  const notificationElement = document.createElement('div');
+  notificationElement.id = 'dddice-notifications';
+  notificationElement.style.top = '0px';
+  notificationElement.style.position = 'fixed';
+  notificationElement.style.pointerEvents = 'none';
+  notificationElement.style.zIndex = '100001';
+  notificationElement.style.opacity = '100';
+  notificationElement.style.height = '100vh';
+  notificationElement.style.width = '100vw';
+  notificationElement.className = 'dddice';
+  notificationElement.style.boxSizing = 'border-box';
 
-// insert notification panel
-const notificationElement = document.createElement('div');
-notificationElement.id = 'dddice-notifications';
-notificationElement.style.top = '0px';
-notificationElement.style.position = 'fixed';
-notificationElement.style.pointerEvents = 'none';
-notificationElement.style.zIndex = '100001';
-notificationElement.style.opacity = '100';
-notificationElement.style.height = '100vh';
-notificationElement.style.width = '100vw';
-notificationElement.className = 'dddice';
-notificationElement.style.boxSizing = 'border-box';
-
-document.body.appendChild(notificationElement);
-const root = ReactDOM.createRoot(document.getElementById('dddice-notifications'));
-
+  document.body.appendChild(notificationElement);
+  root = ReactDOM.createRoot(document.getElementById('dddice-notifications'));
+}
 export default notify;
